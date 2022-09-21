cc81231a99d3102adfa36aa789784357083e79b2
JurTeuwen <jurteuwen@gmail.com>
2022-09-21 10:00:35 +0200

    opdracht 2

--- hfst_1/opdrachten/opdracht_2.py
+++ hfst_1/opdrachten/opdracht_2.py
@@ -9,3 +9,19 @@ scores = [
     [ 7, 6, 5 ], # Life
     [ 6, 5, 6, 6 ] # Different
 ] 
+
+dict = {}
+
+for film in films:
+    for score in scores:
+        dict[film] = score
+        scores.remove(score)
+        break
+
+print(dict)
+
+
+for film,score in dict.items():
+    som = sum(score)
+    gemiddelde = round(som/len(score), 1)
+    print(gemiddelde)    
