2cb8196abf713962373630992794a7edce65d787
JurTeuwen <jurteuwen@gmail.com>
2022-09-21 08:56:03 +0200

    oef 6

--- hfst_1/oefen_mee/oefening_6.py
+++ hfst_1/oefen_mee/oefening_6.py
@@ -6,3 +6,25 @@ engels_nederlands = { "last":"laatste", "week":"week", "the":"de",
 "leidend", "modern":"modern", "composer":"componist",
 "composers":"componisten", "two":"twee", "shed":"schuur",
 "sheds":"schuren" }
+
+
+# # creating a new dictionary
+# my_dict ={"Java":100, "Python":112, "C":11}
+
+# # one-liner
+# print("One line Code Key value: ", list(my_dict.keys())
+# 	[list(my_dict.values()).index(100)])
+
+vertaal = []
+key_value_lijst = list(engels_nederlands.items())
+print(key_value_lijst)
+zin = input().split()
+
+for woord in zin:
+    for key,value in key_value_lijst:
+        if woord == value:
+            vertaal.append(key)
+    vertaal.append(woord)
+
+vertaalde_zin = " ".join(vertaal)
+print(vertaalde_zin)
