449af9751f941a3409dda1e49435a7ad66e47cde
JurTeuwen <jurteuwen@gmail.com>
2022-09-28 09:53:43 +0200

    opdracht_9

--- hfst_1/opdrachten/opdracht_9.py
+++ hfst_1/opdrachten/opdracht_9.py
@@ -9,6 +9,19 @@ puntenlijst = [
     ["Q","Z"]                                           # 7 punten
 ]
 
+def punten_berekenen(puntenlijst):
+    dict_puntenlijst = {}
+    for index,value in enumerate(puntenlijst):
+        for index1,value1 in enumerate(value):
+            dict_puntenlijst[value1] = index+1
+    dict_puntenlijst_2={}
+    for i in sorted(dict_puntenlijst):
+        dict_puntenlijst_2[i]=dict_puntenlijst[i]
+    
+    return dict_puntenlijst_2
+            
+# print(punten_berekenen(puntenlijst))
+
 """ Niveau 2"""
 puntenlijst_en = [
     ["A", "E", "I", "O", "U", "L", "N", "S", "T"],      # 1 punt
@@ -19,3 +32,72 @@ puntenlijst_en = [
     ["J", "X"],                                         # 6 punten
     ["C","Z"]                                           # 7 punten
 ]
+
+def punten_berekenen(puntenlijst):
+    dict_puntenlijst = {}
+    for index,value in enumerate(puntenlijst):
+        for index1,value1 in enumerate(value):
+            dict_puntenlijst[value1] = index+1
+    dict_puntenlijst_2={}
+    for i in sorted(dict_puntenlijst):
+        dict_puntenlijst_2[i]=dict_puntenlijst[i]
+    
+    return dict_puntenlijst_2
+            
+# print(punten_berekenen(puntenlijst_en))
+
+def score_nederlands_dict(punten):
+    totaal = 0
+    dict_puntenlijst = punten_berekenen(punten)
+    woord = input("geef een woord: ")
+
+    for kar in woord:
+        punt = dict_puntenlijst.get(kar)
+        totaal = totaal+punt
+    return totaal
+
+# print(score_nederlands_dict(puntenlijst))
+
+def score_engels_dict(punten):
+    totaal = 0
+    dict_puntenlijst = punten_berekenen(punten)
+    woord = input("geef een woord: ")
+
+    for kar in woord:
+        punt = dict_puntenlijst.get(kar)
+        totaal = totaal+punt
+    return totaal
+
+# print(score_engels_dict(puntenlijst_en))
+
+
+def score_nederlands_list(punten):
+    totaal = 0
+    woord = input("geef een woord: ")
+    for kar in woord:
+        for index,lijst in enumerate(punten):
+            if kar in lijst:
+                punt = index+1
+                totaal = totaal+punt
+    return totaal
+
+# print(score_nederlands_list(puntenlijst))
+
+
+def score_engels_list(punten):
+    totaal = 0
+    woord = input("geef een woord: ")
+    for kar in woord:
+        for index,lijst in enumerate(punten):
+            if kar in lijst:
+                punt = index+1
+                totaal = totaal+punt
+    return totaal
+
+# print(score_nederlands_list(puntenlijst_en))
+
+
+print(score_nederlands_dict(puntenlijst))
+print(score_nederlands_list(puntenlijst))
+print(score_engels_dict(puntenlijst_en))
+print(score_engels_list(puntenlijst_en))
