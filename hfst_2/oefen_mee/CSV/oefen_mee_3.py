bf3575bcb46e095575e96419bec44e3f63a76b36
JurTeuwen <jurteuwen@gmail.com>
2022-10-17 11:16:12 +0200

    j

--- hfst_2/oefen_mee/CSV/oefen_mee_3.py
+++ hfst_2/oefen_mee/CSV/oefen_mee_3.py
@@ -1,6 +1,6 @@
 import csv# open de module csv
 
-fp = open( "volcanic-eruptions-EU.csv", "r" )# open volcanic-eruptions-EU en stop het in variabelen fp
+fp = open( "hfst_2/oefen_mee/CSV/volcanic-eruptions-EU.csv", "r" )# open volcanic-eruptions-EU en stop het in variabelen fp
 # var csv_reader kan je niet rechtstreeks wijzigen of oproepen.
 csv_reader = csv.reader( fp , delimiter=";") # lees de volcanic-eruptions-EU en deel de woorden op na elke ;
 # print(csv_reader[3]) # Voorbeeld
@@ -17,7 +17,7 @@ for eruption in eruptions_ll:
 
 
 
-fp = open("hfst_2/oefen_mee/volcanic-eruptions-EU.csv", "r" )
+fp = open("hfst_2/oefen_mee/CSV/volcanic-eruptions-EU.csv", "r" )
 csv_reader = csv.DictReader( fp , delimiter=";")
 
 eruptions_ld = [] # ld = lijst van dictionaries
