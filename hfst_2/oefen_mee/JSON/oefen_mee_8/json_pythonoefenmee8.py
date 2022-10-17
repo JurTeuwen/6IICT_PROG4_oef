95b688b75b81b68a983655c4c5974a8504c308fa
JurTeuwen <jurteuwen@gmail.com>
2022-10-17 11:41:50 +0200

    oefenmee8

---
+++ hfst_2/oefen_mee/JSON/oefen_mee_8/json_pythonoefenmee8.py
@@ -0,0 +1,11 @@
+from calendar import week
+import json
+
+# Pad is afhankelijk van locatie appel.json
+fp = open("hfst_2/oefen_mee/JSON/oefen_mee_8/oefenmee8.json", "r")
+dagen = json.load(fp)
+fp.close()  # Na sluiten is JSON niet meer bruikbaar
+
+
+dag = input("Welke dag wil je weten: ")
+print(dagen[dag])
