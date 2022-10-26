932505d02a7c3e645b84357300b865d954469939
JurTeuwen <jurteuwen@gmail.com>
2022-10-26 09:23:23 +0200

    12

--- hfst_2/oefen_mee/JSON/oefen_mee_12/oefen_mee12.py
+++ hfst_2/oefen_mee/JSON/oefen_mee_12/oefen_mee12.py
@@ -4,13 +4,16 @@ import json
 fp = open("hfst_2/oefen_mee/JSON/oefen_mee_12/oefenmee12.json", "r")
 dagen = json.load(fp)
 fp.close()  # Na sluiten is JSON niet meer bruikbaar
-
-dag = input("Welke dag wil je weten: ")
-print(dagen[dag])
-aanpassen = input("Wil je dit aanpassen: ")
-if aanpassen == "ja":
-    aanpassing = input(f"Wat ga je doen op {dag}: ")
-    dagen[dag] = aanpassing
-x = open("hfst_2/oefen_mee/JSON/oefen_mee_12/oefenmee12.json", "w")
-json.dump(dagen, x)
-x.close()
+while True:
+    dag = input("Welke dag wil je weten: ")
+    print(dagen[dag])
+    aanpassen = input("Wil je dit aanpassen: ")
+    if aanpassen == "ja":
+        aanpassing = input(f"Wat ga je doen op {dag}: ")
+        dagen[dag] = aanpassing
+    x = open("hfst_2/oefen_mee/JSON/oefen_mee_12/oefenmee12.json", "w")
+    json.dump(dagen, x)
+    x.close()
+    doorgaan = input("Wil je nog iets weten/aanpassen? = ")
+    if doorgaan == "nee":
+        break
