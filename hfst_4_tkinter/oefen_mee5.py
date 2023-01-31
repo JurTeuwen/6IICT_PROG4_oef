5a3b6d26804dcb75d197c59994c0c6af37ead68d
JurTeuwen <jurteuwen@gmail.com>
2023-01-31 17:45:07 +0100

    oefenmee5

--- hfst_4_tkinter/oefen_mee5.py
+++ hfst_4_tkinter/oefen_mee5.py
@@ -1,2 +1,20 @@
-# Zoek de betekenis van een aantal parameters uit.
-# Zijn deze parameters bruikbaar bij Button en/of Label?
+from tkinter import *
+import tkinter
+
+win = Tk()
+
+mbtn = Menubutton(win, text="Courses", relief=RAISED)
+mbtn.grid()
+mbtn.menu = Menu(mbtn, tearoff = 0)
+mbtn["menu"] = mbtn.menu
+
+pythonVar = IntVar()
+javaVar = IntVar()
+phpVar = IntVar()
+
+mbtn.menu.add_checkbutton(label="Python", variable=pythonVar)
+mbtn.menu.add_checkbutton(label="Java", variable=javaVar)
+mbtn.menu.add_checkbutton(label="PHP", variable=phpVar)
+
+mbtn.pack()
+win.mainloop()
