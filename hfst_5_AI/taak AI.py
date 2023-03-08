0bf31c0c43a024011667e6ea6e1ca544a158f5b1
JurTeuwen <jurteuwen@gmail.com>
2023-03-08 08:51:31 +0100

    new

--- hfst_5_AI/taak AI.py
+++ hfst_5_AI/taak AI.py
@@ -1,38 +1,58 @@
-#importing modules
+lijst = ['afrikaans', 'albanian', 'amharic', 'arabic', 'armenian', 'assamese', 'aymara', 'azerbaijani', 'bambara', 'basque', 'belarusian', 'bengali', 'bhojpuri', 'bosnian', 'bulgarian', 'catalan', 'cebuano', 'chichewa', 'chinese (simplified)', 'chinese (traditional)', 'corsican', 'croatian', 'czech', 'danish', 'dhivehi', 'dogri', 'dutch', 'english', 'esperanto', 'estonian', 'ewe', 'filipino', 'finnish', 'french', 'frisian', 'galician', 'georgian', 'german', 'greek', 'guarani', 'gujarati', 'haitian creole', 'hausa', 'hawaiian', 'hebrew', 'hindi', 'hmong', 'hungarian', 'icelandic', 'igbo', 'ilocano', 'indonesian', 'irish', 'italian', 'japanese', 'javanese', 'kannada', 'kazakh', 'khmer', 'kinyarwanda', 'konkani', 'korean', 'krio', 'kurdish (kurmanji)', 'kurdish (sorani)', 'kyrgyz', 'lao', 'latin', 'latvian', 'lingala', 'lithuanian', 'luganda', 'luxembourgish', 'macedonian', 'maithili', 'malagasy', 'malay', 'malayalam', 'maltese', 'maori', 'marathi', 'meiteilon (manipuri)', 'mizo', 'mongolian', 'myanmar', 'nepali', 'norwegian', 'odia (oriya)', 'oromo', 'pashto', 'persian', 'polish', 'portuguese', 'punjabi', 'quechua', 'romanian', 'russian', 'samoan', 'sanskrit', 'scots gaelic', 'sepedi', 'serbian', 'sesotho', 'shona', 'sindhi', 'sinhala', 'slovak', 'slovenian', 'somali', 'spanish', 'sundanese', 'swahili', 'swedish', 'tajik', 'tamil', 'tatar', 'telugu', 'thai', 'tigrinya', 'tsonga', 'turkish', 'turkmen', 'twi', 'ukrainian', 'urdu', 'uyghur', 'uzbek', 'vietnamese', 'welsh', 'xhosa', 'yiddish', 'yoruba', 'zulu']
+tekst = 'Ik ben menno en ik ben dom'
+
+from deep_translator import GoogleTranslator
 import tkinter as tk
-from translate import Translator
-from tkinter import filedialog
 
-tekst = "Ik ben Menno en ik ben dom."
 
-# initializing window
-Screen = tk.Tk()
-Screen.title("vertalen")
+venster = tk.Tk()
 
-InputLanguageChoice = tk.StringVar()
-TranslateLanguageChoice = tk.StringVar()
+taal = ""
 
-#tuple for choosing languages
-LanguageChoices = {'Hausa','Bengali','Hindi','Urdu','Bulgarian','Chinese','Czech','Danish','Dutch','English','Estonian','Finnish','French','German','Greek','Hungarian','Indonesian','Italian','Japanese','Korean','Latvian','Lithuanian','Norwegian','Polish','Portuguese','Romanian','Russian','Slovak','Slovenian','Spanish','Swedish','Turkish','Ukrainian','Arabic','latin'}
-InputLanguageChoice.set('Dutch')
-TranslateLanguageChoice.set('Hindi')
+listbox = tk.Listbox(venster)
+listbox.grid(row=5,column=0)
 
 def Translate():
-    translator = Translator(from_lang= InputLanguageChoice.get(),to_lang=TranslateLanguageChoice.get())
-    Translation = translator.translate(tekst)
-    tk.messagebox.showwarning("vertaling",Translation)
+    global taal
+    translated = GoogleTranslator(source='auto', target=taal).translate(text=tekst)
+    print(translated)
+    tk.messagebox.showinfo(title='vertaalde tekst', message=translated)
 
-#choice for input language
-InputLanguageChoiceMenu = tk.OptionMenu(Screen,InputLanguageChoice,*LanguageChoices)
-tk.Label(Screen,text="Choose a Language").grid(row=0,column=1)
-InputLanguageChoiceMenu.grid(row=1,column=1)
+def cb_search(event):
+      
+    sstr = search_str.get()
+    listbox.delete(0, tk.END)
+    # If filter removed show all data
+    if sstr == "":
+        fill_listbox(lijst) 
+        return
+  
+    filtered_data = list()
+    for item in lijst:
+        if item.find(sstr) >= 0:
+            filtered_data.append(item)
+   
+    fill_listbox(filtered_data)
 
-#choice in which the language is to be translated
-NewLanguageChoiceMenu = tk.OptionMenu(Screen,TranslateLanguageChoice,*LanguageChoices)
-tk.Label(Screen,text="Translated Language").grid(row=0,column=2)
-NewLanguageChoiceMenu.grid(row=1,column=2)
+def fill_listbox(ld):
+    for item in ld:
+        listbox.insert(tk.END, item)
 
-#Button for calling function
-B = tk.Button(Screen,text="Translate",command=Translate, relief = tk.GROOVE).grid(row=3,column=1,columnspan = 3)
+def items_selected(event):
+    global taal
+    # get all selected indices
+    gesilecteerd = listbox.curselection()
+    # get selected items
+    taal = ",".join([listbox.get(i) for i in gesilecteerd])
 
+search_str = tk.StringVar()
+search = tk.Entry(venster, textvariable=search_str, width=10)
+search.grid(row=7,column=0)
+button = tk.Button(venster,text="Translate",command=Translate)
+button.grid(row=10,column=0)
+search.bind('<Return>', cb_search)
+listbox.bind('<<ListboxSelect>>', items_selected)
+
+fill_listbox(lijst)
+venster.title("vertalen")
 tk.mainloop()
