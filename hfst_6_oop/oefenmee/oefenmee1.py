4401b74174cff4026ec65eba358329ec308938f2
JurTeuwen <jurteuwen@gmail.com>
2023-05-22 10:06:37 +0200

    new

--- hfst_6_oop/oefenmee/oefenmee1.py
+++ hfst_6_oop/oefenmee/oefenmee1.py
@@ -2,8 +2,12 @@ class Hond:
     naam = "Rens"
     massa = "20kg"
 
+    def sldkf(self):
+        self.massa = "0kg"
+
 hond = Hond()
 huisdier = Hond()
+hond.sldkf()
 
 print(hond.naam, hond.massa, type(hond))
 print(huisdier.naam, huisdier.massa, type(huisdier))
